607fdb5453565011ec51fc579fd88c776be820bf
wood.wu <kilokingw@gmail.com>
2022-10-11 19:18:07 +0800

    updated 5gpage layout

--- src/Pages/Front/Watch/Watch5gTest.tsx
+++ src/Pages/Front/Watch/Watch5gTest.tsx
@@ -31,7 +31,7 @@ function Watch5gTest() {
 
   return (
     <div className='w-full flex mx-auto mt-15  flex-col md:flex-row items-start'>
-			<div className='streamingvieos w-full aspect-[1/1]  md:w-11/12 md:aspect-[14/10]  relative'>
+			<div className='streamingvieos w-full aspect-[1/1]  md:w-10/12 md:aspect-[14/10]  relative  max-w-[1300px]'>
 				{/*<div className='relative aspect-video flex flex-col'>*/}
 
 				<MultiViewsDumbPlayer {...MultiViewsDumbPlayerSettings} />
@@ -40,7 +40,7 @@ function Watch5gTest() {
 				{/*<div className='bg-zinc-800 w-full h-auto flex justify-center items-center  box-border grow'> View 360 Controller</div>*/}
 				<div className='ar_btn absolute right-8 bottom-[20%] text-2xl font-semibold italic text-zinc-200 hover:text-white cursor-pointer drop-shadow-2xl' >AR</div>
 			</div>
-			<div className='w-full lg:mx-auto lg:w-4/12 h-screen'>
+			<div className='w-full lg:mx-auto lg:w-5/12 max-h-screen'>
 				{
 					!state.isLoggedIn ?
 					<LoginChannel />
