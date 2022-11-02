e7ba25cec4d8c8f419f108c750cd463dec654682
wood.wu <kilokingw@gmail.com>
2022-11-02 14:06:41 +0800

    Update WatchTest.tsx

--- src/Pages/Front/Watch/WatchTest.tsx
+++ src/Pages/Front/Watch/WatchTest.tsx
@@ -30,21 +30,22 @@ function WatchTest() {
 	const { state } = useContext(ChatuserContext);
 
   return (
-    <div className='w-full flex mx-auto mt-15 overflow-hidden h-[calc(100vh-84px)]'>
-			<div className='streamingvieos w-full grow'>
-				{/*<div className='relative aspect-video flex flex-col'>*/}
+    <div className='w-full flex mx-auto mt-15  flex-col lg:flex-row items-start'>
+			<div className='streamingvieos w-full aspect-[1/1] md:aspect-[15/10]  lg:w-10/12 lg:aspect-[14/10]  relative  max-h-[93vh]' style={{touchAction: 'none'}}>
 
 				<MultiViewsDumbPlayer {...MultiViewsDumbPlayerSettings} />
 
-				{/*</div>*/}
-				{/*<div className='bg-zinc-800 w-full h-auto flex justify-center items-center  box-border grow'> View 360 Controller</div>*/}
+				<div className='ar_btn absolute right-4 lg;right-8 top-[70%] text-xl lg:top-[80%] lg:text-2xl font-semibold italic text-zinc-200 hover:text-white cursor-pointer drop-shadow-2xl' >AR</div>
 			</div>
-			{
-				!state.isLoggedIn ?
-				<LoginChannel />
-				:
-				<Channel />
-			}
+			<div className='w-full lg:mx-auto lg:w-3/12 max-h-screen'>
+				{
+					!state.isLoggedIn ?
+					<LoginChannel />
+					:
+					<Channel />
+				}
+			</div>
+
 
 			
 
